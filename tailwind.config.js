37fc298d610b67ba5f0e7999bb758977c4369c52
Alex <30391515+abaum012@users.noreply.github.com>
2020-09-01 14:11:00 -0500

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+        'display': ['Barlow', 'sans-serif'],
+        'body': ['Montserrat', 'sans-serif'],
+    },
+  },
+  variants: {},
+  plugins: [],
+}
